9836c3b3190af6fce9ab422bcb06659991cdc41f
Mikancat10 <mikancat09@gmail.com>
2026-02-15 20:42:11 +0900

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,4 +1,6 @@
 const Storage={
  load(){return JSON.parse(localStorage.getItem("fastmemo")||"[]")},
- save(data){localStorage.setItem("fastmemo",JSON.stringify(data))}
+ save(data){localStorage.setItem("fastmemo",JSON.stringify(data))},
+ loadFont(){return localStorage.getItem("fastmemo_font")||"system-ui"},
+ saveFont(f){localStorage.setItem("fastmemo_font",f)}
 };
